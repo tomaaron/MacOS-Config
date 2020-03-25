92a4e55d20d3b4c2f17105d47456cc4ae7eaea06
Tom Aaron Koch <t.koch@tu-berlin.de>
2020-03-25 10:08:02 +0100

    Add my apps

--- BrewCask.sh
+++ BrewCask.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-#Check If Brew Is Installed
-if ! [ -x "$(command -v brew)" ]; then
-      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-   else
-     brew update
-     brew upgrade
-   fi
-   
 # Install Need Software on Mac #
-#brew cask install evernote
 brew cask install firefox
-#brew cask install visual-studio-code
-#brew cask install google-chrome
-#brew cask install peakhour
-brew cask install dropbox
-brew cask install telegram
-#brew cask install 1password
+brew cask install vscodium
+brew cask install google-chrome
+brew cask install rocket-chat
 brew cask install iterm2
-#brew cask install vmware-fusion
-#brew cask install ipvanish-vpn
-brew cask install insomniax
-brew tap buo/cask-upgrade
+brew cask install docker
+brew cask install alfred
+brew cask install cheatsheet
+brew cask install notion
+brew cask install openoffice
+brew cask install slack
+brew cask install spotify
+brew cask install spectacle
+brew cask install mysqlworkbench
